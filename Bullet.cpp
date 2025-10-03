f4edec4e993efeb6f9ea0ccc3f227e88ffd36d0d
Leo Xu <leo.xu.64@gmail.com>
2025-10-02 21:59:07 -0400

    bullet class updates

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,1 +1,4 @@
 #include "Bullet.h"
+
+Bullet::Bullet(float x, float y, int w, int h, bool friendly)
+    : GameObject(x, y, w, h), friendly(friendly) {}
--- Bullet.h
+++ Bullet.h
@@ -6,6 +6,10 @@
 class Bullet : public GameObject {
 private:
     bool friendly;
+    float direction;
 public:
-    Bullet(float x, float y, int w, int h);
+    Bullet(float x, float y, int w, int h, bool friendly);
+    void update();
+    void render(SDL_Renderer* renderer);
+    
 };
